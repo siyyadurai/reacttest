ea1d099c85b090028d326ee4fd47b08d55343678
siyyadurai <42440003+siyyadurai@users.noreply.github.com>
2018-08-21 18:08:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody!');
-  
+res.end('Hi everybody!');  
 });
 server.listen(8082);
